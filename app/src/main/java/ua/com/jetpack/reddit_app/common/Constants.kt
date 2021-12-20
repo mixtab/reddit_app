c0a4fab8e5097a83a3795d38205d9111dff8f518
mixtab <m.tabarkevych@gmail.com>
2021-12-20 18:52:12 +0200

    Add constants

--- app/src/main/java/ua/com/jetpack/reddit_app/common/Constants.kt
+++ app/src/main/java/ua/com/jetpack/reddit_app/common/Constants.kt
@@ -1,7 +1,10 @@
 package ua.com.jetpack.reddit_app.common
 
 object Constants {
-
-
+    //BASE
     const val BASE_URL = "https://www.reddit.com"
+
+    //TIME
+    const val FORMAT_TIME_UA = "yyyy-MM-dd HH:mm:ss"
+
 }
